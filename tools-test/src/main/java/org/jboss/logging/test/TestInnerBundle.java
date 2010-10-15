d4a9c5538dcea944a7b2a07ad6d9aa4c3c483765
James Perkins <jrperkinsjr@gmail.com>
2010-10-15 12:12:36 -0700

    Add class for error checking.

--- tools-test/src/main/java/org/jboss/logging/test/TestInnerBundle.java
+++ tools-test/src/main/java/org/jboss/logging/test/TestInnerBundle.java
@@ -3,11 +3,8 @@
  */
 package org.jboss.logging.test;
 
-import java.io.IOException;
 
 import org.jboss.logging.Cause;
-import org.jboss.logging.LogMessage;
-import org.jboss.logging.Logger.Level;
 import org.jboss.logging.Message;
 import org.jboss.logging.Message.Format;
 import org.jboss.logging.MessageBundle;
@@ -18,24 +15,11 @@ import org.jboss.logging.MessageBundle;
  */
 public class TestInnerBundle {
 
-    @MessageBundle(projectCode = "LOGB")
-    static interface InnerBundle {
+    @MessageBundle
+    private interface InnerBundle {
 
-        @LogMessage(level = Level.ERROR)
-        @Message("%s cannot be null.")
-        String valueNotNull(String value);
-
-        @LogMessage(level = Level.ERROR)
-        @Message(id = 1, value = "Version: %s")
-        String version(String version);
-
-        @LogMessage(level = Level.FATAL)
-        @Message(id = 2, value = "This is not good %s is melting.")
-        RuntimeException meltDown(@Cause IOException cause, String value);
-
-        @LogMessage(level = Level.WARN)
-        @Message(id = 3, value = "Value {0} could not be added to {1}",
+        @Message(id = 20, value = "ERROR: {0} Root cause: {1}",
                  format = Format.MESSAGE_FORMAT)
-        String invalidValue(@Cause Throwable cause);
+        String errorMessage(@Cause Throwable cause, String message);
     }
 }
