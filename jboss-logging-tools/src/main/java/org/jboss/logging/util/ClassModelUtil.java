4f7985b9c2ee3a7be36a32f38e81b4b508cfd616
kevinpollet <pollet.kevin@gmail.com>
2010-10-25 10:57:54 +0200

    Move the ClassModel util.

--- jboss-logging-tools/src/main/java/org/jboss/logging/util/ClassModelUtil.java
+++ jboss-logging-tools/src/main/java/org/jboss/logging/util/ClassModelUtil.java
@@ -18,7 +18,7 @@
  *  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  *  site: http://www.fsf.org.
  */
-package org.jboss.logging.model;
+package org.jboss.logging.util;
 
 import com.sun.codemodel.internal.JDefinedClass;
 import com.sun.codemodel.internal.JExpr;
