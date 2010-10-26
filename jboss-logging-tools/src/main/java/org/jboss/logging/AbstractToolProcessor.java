654def52efa02eb63e987dfdf31c16dfb7dee2dc
James Perkins <jrperkinsjr@gmail.com>
2010-10-26 05:23:56 -0700

    Renamed generator class and added validation processor.

---
+++ jboss-logging-tools/src/main/java/org/jboss/logging/AbstractToolProcessor.java
@@ -0,0 +1,195 @@
+/*
+ * JBoss, Home of Professional Open Source Copyright 2010, Red Hat, Inc., and
+ * individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.jboss.logging;
+
+import java.util.ArrayList;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedOptions;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.ElementFilter;
+
+/**
+ * An abstract processor used process annotations.
+ * 
+ * @author James R. Perkins Jr. (jrp)
+ * @author Kevin Pollet
+ */
+public abstract class AbstractToolProcessor {
+
+    private final Elements elementUtils;
+
+    private final Filer filer;
+
+    private final ToolLogger logger;
+
+    private final Types typeUtils;
+
+    private final ProcessingEnvironment processingEnv;
+
+    /**
+     * Constructs a new processor.
+     *
+     * @param processingEnv the processing environment.
+     */
+    public AbstractToolProcessor(final ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+        this.elementUtils = processingEnv.getElementUtils();
+        this.filer = processingEnv.getFiler();
+        this.typeUtils = processingEnv.getTypeUtils();
+        this.logger = ToolLogger.getLogger(this.getClass(), processingEnv.
+                getMessager(), processingEnv.getOptions());
+    }
+
+    /**
+     * Processes classes annotated with the
+     * {@link org.jboss.logging.MessageBundle} or
+     * {@link org.jboss.logging.MessageLogger}/
+     *
+     * @param annotations the to process.
+     * @param roundEnv    the round environment.
+     */
+    public void process(final Set<? extends TypeElement> annotations,
+            final RoundEnvironment roundEnv) {
+        process(ElementFilter.typesIn(roundEnv.getElementsAnnotatedWith(
+                MessageBundle.class)));
+        process(ElementFilter.typesIn(roundEnv.getElementsAnnotatedWith(
+                MessageLogger.class)));
+    }
+
+    /**
+     * Processes the types.
+     *
+     * @param annotations the to process.
+     */
+    public void process(
+            final Collection<? extends TypeElement> elementsToProcess) {
+        for (TypeElement element : elementsToProcess) {
+            process(ElementFilter.typesIn(element.getEnclosedElements()));
+            final List<ExecutableElement> methods = new ArrayList<ExecutableElement>();
+            if (element.getKind().isInterface() && !element.getModifiers().
+                    contains(
+                    Modifier.PRIVATE)) {
+                for (TypeMirror interfaceType : element.getInterfaces()) {
+                    methods.addAll(ElementFilter.methodsIn(typeUtils.asElement(
+                            interfaceType).getEnclosedElements()));
+                }
+                methods.addAll(ElementFilter.methodsIn(
+                        element.getEnclosedElements()));
+                processMethods(element, methods);
+            } else {
+                logger().warn(
+                        "Type %s must be an interface with at least package-private access. Skipping processing.",
+                        element, element.getQualifiedName().toString());
+            }
+        }
+    }
+
+    /**
+     * Processes the methods in the interface.
+     *
+     * @param element the element that contains the methods.
+     * @param methods the methods in the interface.
+     */
+    public abstract void processMethods(final TypeElement element,
+            final Collection<ExecutableElement> methods);
+
+    /**
+     * Returns the logger to log messages with.
+     *
+     * @return the logger to log messages with.
+     */
+    public final ToolLogger logger() {
+        return logger;
+    }
+
+    /**
+     * Returns the filer.
+     *
+     * @return the filer
+     */
+    public Filer filer() {
+        return this.filer;
+    }
+
+    /**
+     * Returns the element utils.
+     *
+     * @return the utils
+     */
+    public Elements elementUtils() {
+        return this.elementUtils;
+    }
+
+    /**
+     * Returns the type utils.
+     *
+     * @return the utils
+     */
+    public Types typeUtils() {
+        return this.typeUtils;
+    }
+
+    /**
+     * Returns the processing environment.
+     *
+     * @return the processing environment being used.
+     */
+    public final ProcessingEnvironment processingEnv() {
+        return processingEnv;
+    }
+
+    /**
+     * Returns the name of the processor.
+     *
+     * @return the name of the processor.
+     */
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    /**
+     * Returns the supported options set.
+     *
+     * @return the supported options set or empty set if none
+     */
+    public final Set<String> getSupportedOptions() {
+        SupportedOptions options = this.getClass().getAnnotation(
+                SupportedOptions.class);
+        if (options != null) {
+            return new HashSet(Arrays.asList(options.value()));
+        }
+
+        return Collections.EMPTY_SET;
+    }
+}
---
+++ jboss-logging-tools/src/main/java/org/jboss/logging/validation/ValidationProcessor.java
@@ -0,0 +1,53 @@
+/*
+ *  JBoss, Home of Professional Open Source Copyright 2010, Red Hat, Inc., and
+ *  individual contributors by the @authors tag. See the copyright.txt in the
+ *  distribution for a full listing of individual contributors.
+ * 
+ *  This is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation; either version 2.1 of the License, or (at your option)
+ *  any later version.
+ * 
+ *  This software is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ *  details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this software; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ *  site: http://www.fsf.org.
+ */
+package org.jboss.logging.validation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import org.jboss.logging.AbstractToolProcessor;
+
+/**
+ * Runs validation processes.
+ * 
+ * @author James R. Perkins (jrp)
+ */
+public class ValidationProcessor extends AbstractToolProcessor {
+
+    private final List<Validator> validators;
+
+    public ValidationProcessor(final ProcessingEnvironment processingEnv) {
+        super(processingEnv);
+        this.validators = new ArrayList<Validator>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void processMethods(final TypeElement element,
+            final Collection<ExecutableElement> methods) {
+        validators.add(new MessageIdValidator(methods));
+    }
+}
