4fbb9d490472829d419b2e6fc691091358947d23
James Perkins <jrperkinsjr@gmail.com>
2010-12-21 08:07:54 -0800

    Various testing changes.

--- tools-test/src/main/java/org/jboss/logging/test/TestLogger.java
+++ tools-test/src/main/java/org/jboss/logging/test/TestLogger.java
@@ -17,7 +17,7 @@ import org.jboss.logging.MessageLogger;
 @MessageLogger(projectCode = "LOGL")
 public interface TestLogger {
 
-    void valueNotNull(Object value);
+    String valueNotNull(Object value);
 
     @LogMessage(level = Level.WARN)
     @Message(id = 1, value = "%s cannot be null.")
@@ -27,15 +27,14 @@ public interface TestLogger {
     void valueNotNull(@Cause IllegalArgumentException cause, Object value);
 
     @LogMessage(level = Level.INFO)
-    @Message(id = 3, value = "Version: %s")
+    @Message(id = 2, value = "Version: %s")
     void version(String version);
 
     @LogMessage(level = Level.FATAL)
-    @Message(id = 2, value = "This is not good %s is melting.")
+    @Message(id = 3, value = "This is not good %s is melting.")
     void meltDown(@Cause Throwable cause, String value);
 
-    @LogMessage(level = Level.WARN)
     @Message(id = 4, value = "Value {0} could not be added to {1}",
              format = Format.MESSAGE_FORMAT)
-    void invalidValue(@Cause Throwable cause, String value, String collection);
+    String invalidValue(@Cause Throwable cause, String value, String collection);
 }
