c686882f8191863103be313d24b43de014d8849b
James Perkins <jrperkinsjr@gmail.com>
2010-12-23 11:09:59 -0800

    Various testing changes.

--- tools-test/src/main/java/org/jboss/logging/test/TestExtendedLogger.java
+++ tools-test/src/main/java/org/jboss/logging/test/TestExtendedLogger.java
@@ -14,6 +14,7 @@ import org.jboss.logging.MessageLogger;
  */
 @MessageLogger(projectCode = "LOGEL")
 public interface TestExtendedLogger extends TestLogger {
+
     @LogMessage(level = Level.ERROR)
     @Message(id = Message.INHERIT, value = "%s.")
     void extendedError(String value);
