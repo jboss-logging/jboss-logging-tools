60c341299f893b5b5ea1d6a63d21a65fc455468f
James Perkins <jperkins@redhat.com>
2012-04-24 14:45:18 -0700

    [LOGTOOL-45] Allow a no format option for message bundles and logged messages.

---
+++ src/main/java/org/jboss/logging/processor/validation/NoFormatValidator.java
@@ -0,0 +1,45 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.logging.processor.validation;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+final class NoFormatValidator extends AbstractFormatValidator {
+    static final NoFormatValidator INSTANCE = new NoFormatValidator();
+
+    @Override
+    public int argumentCount() {
+        return 0;
+    }
+
+    @Override
+    public String format() {
+        return "none";
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+}
