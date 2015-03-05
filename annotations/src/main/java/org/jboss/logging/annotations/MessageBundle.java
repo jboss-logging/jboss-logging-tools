2e578c83de5f53b73c48c5717f4903b1bbd01c3b
James Perkins <jperkins@redhat.com>
2015-03-05 15:09:36 -0800

    Fix license headers and license references in POM.

--- annotations/src/main/java/org/jboss/logging/annotations/MessageBundle.java
+++ annotations/src/main/java/org/jboss/logging/annotations/MessageBundle.java
@@ -1,7 +1,7 @@
 /*
  * JBoss, Home of Professional Open Source.
  *
- * Copyright 2010 Red Hat, Inc., and individual contributors
+ * Copyright 2015 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
